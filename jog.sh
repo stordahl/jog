24ae306be00c07d4a3b69c24b7648892375969a1
stordahl <jacob.stordahl@stylitics.com>
2023-05-20 18:35:20 -0500

    feat(integration): add pnpm support
    Jog can now automatically detect pnpm configuration files to run commands with pnpm, rather than npm

--- jog.sh
+++ jog.sh
@@ -7,7 +7,33 @@
 selected=`jq -r '.scripts | keys[]' package.json | fzf`
 
 if [[ -z $selected ]]; then
-  exit 0
+  echo "JOG ERROR: no npm scripts found"
+  exit 1
 fi
 
-npm run $selected
+# Define the array of file paths
+file_paths=(
+  "pnpm-lock.yaml"
+  "pnpm-workspace.yaml"
+  ".pnpmfile.cjs"
+)
+
+# Function to check if any file exists
+check_file_exists() {
+  local files=("$@")  # Store the arguments in an array
+
+  for file in "${files[@]}"; do
+    if [ -e "$file" ]; then
+      echo "JOG LOG: Identified pnpm configuration"
+      pnpm run $selected
+      return 0
+    fi
+  done
+
+  echo "JOG LOG: Identified npm configuration"
+  npm run $selected
+  return 0
+}
+
+# Call the function with the file_paths array
+check_file_exists "${file_paths[@]}"
